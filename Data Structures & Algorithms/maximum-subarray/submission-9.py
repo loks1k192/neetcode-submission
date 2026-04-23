16c05457abaf4a9770d40bd065a5742228f0692e
Nikolay Knyazkov <146207386+loks1k192@users.noreply.github.com>
2026-04-24 02:48:50 +0300

    Add: maximum-subarray - submission-9

---
+++ Data Structures & Algorithms/maximum-subarray/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+	def maxSubArray(self, nums: List[int]) -> int:
+		if len(nums) == 1:
+			return nums[0]
+		mn = 0
+		mx = nums[0]
+		s = 0
+		#pref_sums.append(nums[i] + pref_sums[i])
+		for i in range(len(nums)):
+			s += nums[i]
+			mx = max(mx, s - mn)
+			mn = min(mn, s)
+		return mx
